69353ca6e80e0e8106a85e916dee3b46d27d3d5c
mlerena <manuellerena@gmail.com>
2025-10-17 11:25:57 -0300

    fix(dependencies): update gulp to version 4.0.2 and refactor gulp tasks for sequential execution

--- gulpfile.js
+++ gulpfile.js
@@ -2,45 +2,46 @@
 
 const gulp = require('gulp');
 const insert = require('gulp-insert');
-const fs= require('fs');
+const fs = require('fs');
 
 const remap = fs.readFileSync('src/common/src/cordova-remap.js', 'utf-8');
 
-function webpack(config, callback){
-  const exec = require('child_process').exec;
-  exec(__dirname + '/node_modules/.bin/webpack --config ' + config, (error, stdout, stderr) => {
-    console.log(stdout);
-    console.log(stderr);
-    callback(error);
-  });
+function webpackTask(config) {
+  return function (cb) {
+    const exec = require('child_process').exec;
+    exec(__dirname + '/node_modules/.bin/webpack --config ' + config, (error, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+      cb(error);
+    });
+  };
 }
 
-gulp.task('prepack', function(cb){
-  webpack('webpack.prepack.config.js', cb);
-});
+const prepack = webpackTask('webpack.prepack.config.js');
+const webpackCordova = webpackTask('webpack.cordova.config.js');
+const distTask = webpackTask('webpack.library.config.js');
 
-gulp.task('webpack-cordova', ['prepack'], function(cb){
-  webpack('webpack.cordova.config.js', cb);
-});
-
-gulp.task('dist', ['prepack'], function(cb){
-  webpack('webpack.library.config.js', cb);
-});
-
-gulp.task('remap', ['webpack-cordova'], function () {
+function remapTask() {
   return gulp.src(['dist/plugin.min.js', 'dist/www.min.js'])
-  .pipe(insert.prepend(remap))
-  .pipe(gulp.dest('dist'));
-});
+    .pipe(insert.prepend(remap))
+    .pipe(gulp.dest('dist'));
+}
 
-gulp.task('plugin', ['remap'], function () {
+function pluginTask() {
   return gulp.src(['dist/plugin.min.js'])
-  .pipe(gulp.dest('src/browser'));
-});
+    .pipe(gulp.dest('src/browser'));
+}
 
-gulp.task('www', ['remap'], function () {
+function wwwTask() {
   return gulp.src(['dist/www.min.js'])
-  .pipe(gulp.dest('www'));
-});
+    .pipe(gulp.dest('www'));
+}
 
-gulp.task('default', ['dist', 'plugin', 'www']);
+// Compose tasks using gulp.series for sequential execution
+gulp.task('prepack', prepack);
+gulp.task('webpack-cordova', gulp.series('prepack', webpackCordova));
+gulp.task('dist', gulp.series('prepack', distTask));
+gulp.task('remap', gulp.series('webpack-cordova', remapTask));
+gulp.task('plugin', gulp.series('remap', pluginTask));
+gulp.task('www', gulp.series('remap', wwwTask));
+gulp.task('default', gulp.series('dist', 'plugin', 'www'));
